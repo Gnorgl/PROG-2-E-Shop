61b897b329116765b275f4e540ba8f003a4fd640
KP <vn.pham_khang@yahoo.de>
2026-04-25 15:51:43 +0200

    Update

---
+++ src/main/java/entities/Kunde.java
@@ -0,0 +1,7 @@
+package entities;
+
+public class Kunde extends Benutzer{
+    public Kunde (String email, String passwort, String nachname,String vorname) {
+        super(email, passwort, nachname, vorname);
+    }
+}
