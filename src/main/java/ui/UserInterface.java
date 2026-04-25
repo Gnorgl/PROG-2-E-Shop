92b06672ed752e3bdcfa23bc6dedcebafd242fef
Gnorgel <rijschroeder@gmail.com>
2026-04-25 14:15:14 +0200

    Update UserInterface.java

--- src/main/java/ui/UserInterface.java
+++ src/main/java/ui/UserInterface.java
@@ -2,6 +2,9 @@ package ui;
 
 public class UserInterface {
     public static void main(String[] args) {
+
         System.out.println("Dies ist das UserInterface");
+        System.out.println("Test");
     }
+
 }
