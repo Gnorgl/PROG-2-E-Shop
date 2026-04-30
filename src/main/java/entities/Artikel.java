4bf3d25d29bb4c4517dea6feb47f4158e386b7d9
Gianpsd <gianisikli1@gmail.com>
2026-04-30 11:38:50 +0200

    v

--- src/main/java/entities/Artikel.java
+++ src/main/java/entities/Artikel.java
@@ -1,4 +1,23 @@
 package entities;
 
 public class Artikel {
+    private int artikelNummer;
+    private string bezeichnung;
+    private int bestand;
+}
+
+public Artikel (int artikelNummer, string bezeichnung, int bestand) {
+    this.artikelNummer = artikelNummer;
+    this.bezeichnung = bezeichnung;
+    this.bestand = bestand;
+}
+public int getartikelNummer() {
+    return artikelNummer;
+}
+public string getbezeichnung() {
+    retunr bezeichnung;
+}
+
+public void setbezeichnung (string bezeichnung) {
+    this.bezeichnung = bezeichnung;
 }
