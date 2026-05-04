458fa8b745ba594b6495470d5d98bd8cd7fb4f41
Gianpsd <gianisikli1@gmail.com>
2026-05-04 15:52:57 +0200

    v

--- src/main/java/logic/ArtikelManager.java
+++ src/main/java/logic/ArtikelManager.java
@@ -1,6 +1,17 @@
 package logic;
 
+import entities.Artikel;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArtikelManager {
+    private List<> artikelbestand = new ArrayList<>()
+            @return
+
+    public Artikel legeArtikelan (int nr, String name, int bestand, double preis)
+        Artikel neuerArtikel = new Artikel (nr, name, bestand,preis);
+    artikelBestand.add(neuer Artikel);
+    return neuerArtikel;
 }
 
 //Wird genutzt, um neue Artikel zu erstellen
