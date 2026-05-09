f64a774217f10da130444cadc53f6f07320c7da0
Gnorgel <rijschroeder@gmail.com>
2026-05-09 19:45:18 +0200

    update interface IMV

--- src/main/java/logic/management/MitarbeiterVerwaltung.java
+++ src/main/java/logic/management/MitarbeiterVerwaltung.java
@@ -4,17 +4,11 @@ import logic.moduls.IMV;
 
 public class MitarbeiterVerwaltung implements IMV {
 
-
-
     @Override
     public boolean createNewMitarbeiter() {
         return true;
     }
 
-    @Override
-    public void artikelAnlegen() {
-
-    }
 
 
 
--- src/main/java/logic/moduls/IMV.java
+++ src/main/java/logic/moduls/IMV.java
@@ -4,5 +4,4 @@ public interface IMV {
 
     boolean createNewMitarbeiter();
 
-    void artikelAnlegen();
 }
