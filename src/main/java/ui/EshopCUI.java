3eb2de6917b81d288069fe5c54ae11111bb7aa8c
Gnorgel <rijschroeder@gmail.com>
2026-05-15 17:27:26 +0200

    Update EshopCUI.java

--- src/main/java/ui/EshopCUI.java
+++ src/main/java/ui/EshopCUI.java
@@ -1,6 +1,7 @@
 package ui;
 
 import entities.Benutzer;
+import entities.Mitarbeiter;
 import logic.Eshop;
 
 
@@ -33,18 +34,31 @@ public class EshopCUI {
     }
 
     private void menu() {
-        System.out.println("------MENÜ------");
+        //Nicht angemeldete Benutzer:
         if (this.angemeldeterBenutzer == null) {
-            System.out.println("Login: l");
-            System.out.println("Registrieren: r");
+            System.out.println("[L] Login");
+            System.out.println("[R] Registrieren");
+            System.out.println("[E] Beenden");
         } else {
-            System.out.println("Wählen Sie aus:");
-            System.out.println("Warenkatalog: w");
-            System.out.println("Bestellverlauf: b");
-            System.out.println("Kundensupport: k");
-            System.out.println("Abmelden: a");
+            //Angemeldete Benutzer:
+            System.out.println("----------------");
+            System.out.format("Willkommen im Eshop %s (%s)\n",
+                    angemeldeterBenutzer.getVorname(),
+                    angemeldeterBenutzer instanceof entities.Mitarbeiter ? "Mitarbeiter" : "Kunde");
+            System.out.println("----------------");
+            System.out.println("[W] Warenkatalog");
+            //Benutzer ist Mitarbeiter:
+            if (angemeldeterBenutzer instanceof entities.Mitarbeiter) {
+                System.out.println("[P] Produkt hinzufügen");
+                System.out.println("[I] Orderverlauf einsehen");
+            } else {
+                //Benutzer ist Kunde:
+                System.out.println("[B] Bestellverlauf");
+                System.out.println("[S] Support");
+            }
+            System.out.println("[A] Abmelden");
         }
-        System.out.println("Beenden: e");
+        System.out.print("> ");
     }
 
     private void navigation(String input) {
@@ -56,60 +70,73 @@ public class EshopCUI {
             case "w" -> warenkatalog();
             case "b" -> bestellverlauf();
             case "a" -> logout();
-            case "k" -> kundensupport();
+            case "s" -> kundensupport();
             case "e" -> programmBeenden();
             default -> {
+                System.out.println("----------------");
                 System.out.println("Unbekannter Befehl!");
+                System.out.println("----------------");
             }
         }
     }
 
     private void benutzerErstellen() {
+        System.out.println("----------------");
         System.out.println("Erstellen Sie einen Benutzer!");
+        System.out.println("----------------");
     }
 
     private void login() {
-        System.out.println("Anmelden:");
+        System.out.println("------Login------");
         System.out.println("E-Mail:");
 
         String email = scanner.nextLine();
 
         Benutzer benutzer = eshop.getBenutzerVerwaltung().benutzerCheck(email);
-
+        //Technisch gesehen hier ein Loop, falls man sich verschreibt.
+        //Aber sonst muss es mehrere Eingaben geben damit man zum Anfang zurückkommt.
+        //Dies wird implementiert, wenn wir Buttons im GUI benutzen.
         if (benutzer != null) {
             System.out.println("Passwort:");
             String passwort = scanner.nextLine(); //Darf nicht empty sein!
 
             if (eshop.getBenutzerVerwaltung().passwordCheck(benutzer, passwort)) {
                 this.angemeldeterBenutzer = benutzer;
-                System.out.println("Willkommen zurück " + this.angemeldeterBenutzer.getVorname());
             } else {
+                System.out.println("----------------");
                 System.out.println("Falsches Passwort!");
+                System.out.println("----------------");
             }
         } else {
+            System.out.println("----------------");
             System.out.println("Benutzer nicht gefunden!");
+            System.out.println("----------------");
         }
     }
 
     private void logout() {
         this.angemeldeterBenutzer = null;
+        System.out.println("----------------");
         System.out.println("Sie wurden abgemeldet!");
+        System.out.println("----------------");
     }
 
     private void warenkatalog() {
-        System.out.println("Warenkatalog:");
+        System.out.println("------Warenkatalog------");
     }
 
     private void bestellverlauf() {
-        System.out.println("Bestellverlauf:");
+        System.out.println("------Bestellverlauf------");
     }
 
     private void kundensupport() {
-        System.out.println("Kontaktmöglichkeiten:");
+        System.out.println("------Kontaktmöglichkeiten------");
     }
 
     private void programmBeenden() {
+        System.out.println("----------------");
         System.out.println("Programm wird beendet!");
+        System.out.println("----------------");
         this.running = false;
     }
 
@@ -117,6 +144,7 @@ public class EshopCUI {
     public static void main(String[] args) {
         Eshop eshop = new Eshop();
         EshopCUI eShopCUI = new EshopCUI(eshop);
+        eshop.getBenutzerVerwaltung().getMitarbeiterVerwaltung().createNewMitarbeiter("admin@email.com", "123", "AI", "Admin");
         eShopCUI.start();
 
     }
