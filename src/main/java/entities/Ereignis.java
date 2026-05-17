6e930964e13af389e7ac59098c392509253045dd
Gianpsd <gianisikli1@gmail.com>
2026-05-17 13:32:58 +0200

    v

---
+++ src/main/java/entities/Ereignis.java
@@ -0,0 +1,18 @@
+package entities;
+import java.time.LocalDate;
+
+public class Ereignis {
+    private int datum //Jahrestag
+    private Artikel artikel;
+    private int anzahl;
+    private Benutzer benutzer; //Kunde oder Mitarbeiter
+    private String typ;
+
+    public Ereignis(Artikel artikel, int anzahl, Benutzer benutzer, String typ) {
+        this.datum = LocalDate.now().getDayOfYear();
+        this.artikel = artikel;
+        this.anzahl = anzahl;
+        this.benutzer = benutzer;
+        this.typ = typ;
+    }
+}
