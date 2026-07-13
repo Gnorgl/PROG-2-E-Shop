8cb2b9e7900df8a17325070928c5866059da6cd4
Gnorgel <rijschroeder@gmail.com>
2026-07-13 19:36:42 +0200

    Update InterfaceEshop.java

--- Common/src/main/java/interfaces/InterfaceEshop.java
+++ Common/src/main/java/interfaces/InterfaceEshop.java
@@ -3,7 +3,9 @@ package interfaces;
 import interfaces.moduls.*;
 
 public interface InterfaceEshop extends IAV, IBV, ICV, IKV, IMV, IOV, IUC, IWV {
-    //Kann leer bleiben. Wir müssen nur die ganzen Interfaces um die wichtigen Methoden aus den Verwaltungsklassen ergänzen.
+    //Kann leer bleiben. Also hier nichts reinschreiben.
+    //
+    // Wir müssen nur die ganzen moduls-Interfaces um die wichtigen Methoden aus den Verwaltungsklassen ergänzen.
     //Also alle Logik Methoden, die in den GUI Klassen direkt aufgerufen werden, müssen mit @Override markiert sein und müssen
-    //sich in den moduls Interfaces befinden!
+    //sich in den moduls-Interfaces befinden!
 }
